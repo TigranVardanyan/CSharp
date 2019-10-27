f8d0a1b5384bc0508fc530889d97874558bd936e
Tigran <tig.vardanyan.001@gmail.com>
2019-10-27 11:50:01 +0400

    vote app

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Pleas enter your name \n");
+            string name = Console.ReadLine();
+            Console.Write("Pleas enter your age \n");
+            while(true)
+            {
+                try
+                {
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    if (age >= 18)
+                    {
+                        Console.Write("You have the right to vote");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You dont have the right to vote");
+                    }
+                    break;
+                } catch
+                {
+
+                    Console.Write("Pleas enter your real age \n");
+                }
+            }
+            
         }
     }
 }
